d29cc5ad42ddb4f687bb62dc49d1737349cf6968
Shubham Singh <imshubhamsingh97@gmail.com>
2017-01-12 06:29:03 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname));
-
-app.set('port', (process.env.PORT || 5000));
-
-app.listen(app.get('port'), function () {
-});
