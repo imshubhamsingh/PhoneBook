3d57af76eb11f68491c1051013906f1d727fe786
Shubham Singh <imshubhamsingh97@gmail.com>
2017-01-13 08:24:48 +0530

    modified controller and config files

---
+++ assets/js/config.js
@@ -0,0 +1,32 @@
+/* configuring routes for the view */
+app.config(function ($routeProvider, $locationProvider, cfpLoadingBarProvider) {
+    // remove hash from URL
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    })
+
+    // for angular-loader
+    cfpLoadingBarProvider.includeSpinner = false
+
+    $routeProvider
+        .when('/', {
+            controller: 'indexCtl',
+            templateUrl: '../../views/contactList.html'
+        })
+        .when('/add-contact', {
+            controller: 'addCtl',
+            templateUrl: '../../views/addContact.html'
+        })
+        .when('/contact-' + ':id', {
+            controller: 'infoCtl',
+            templateUrl: '../../views/contactCheck.html'
+        })
+        .when('/about', {
+            controller: 'aboutCtl',
+            templateUrl: '../../views/about.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+})
